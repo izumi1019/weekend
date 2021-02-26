ad4e81aacc7c987d914e0c75e680814e645f341e
izumi1019 <khm1_frog@outlook.jp>
2021-02-26 12:22:15 +0900

    menu done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_24_032332) do
+ActiveRecord::Schema.define(version: 2021_02_26_002157) do
+
+  create_table "posts", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "title"
+    t.string "sub_title"
+    t.text "body"
+    t.string "image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
